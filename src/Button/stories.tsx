6134074fc3d1d090171590e41072d6a60bddd057
Harrison1 <hmcguire13@gmail.com>
2019-03-22 15:02:57 -0700

    portal

--- src/Button/stories.tsx
+++ src/Button/stories.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import { withKnobs, text, boolean } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
-import Button from './Button';
+import Button from './Button'
+import Portal from '../Portal'
 
 const stories = storiesOf('Button', module)
 
@@ -9,17 +10,19 @@ stories.addDecorator(withKnobs)
 
 stories
 	.add('with text', () => (
-		<Button
-			aria-label={text('Aria Label', 'My Button')}
-			autoFocus={boolean('Auto Focus', false)}
-			disabled={boolean('Disabled', false)}
-			fontSize={text('Font Size', '1rem')}
-			name={text('Name', 'Galaxy')}
-			type={text('Type', 'button')}
-			value={text('Value', '1')}
-		>
-			{text('Button Text', 'Hello Galaxy')}
-		</Button>
+		<Portal id="dialog-box">
+			<Button
+				aria-label={text('Aria Label', 'My Button')}
+				autoFocus={boolean('Auto Focus', false)}
+				disabled={boolean('Disabled', false)}
+				fontSize={text('Font Size', '1rem')}
+				name={text('Name', 'Galaxy')}
+				type={text('Type', 'button')}
+				value={text('Value', '1')}
+			>
+				{text('Button Text', 'Hello Galaxy')}
+			</Button>
+		</Portal>
 	))
 	.add('with emoji', () => (
 		<Button>🚿 🚿 🐈 🐈</Button>
---
+++ src/Dialog/Dialog.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+// import * as ReactDom from 'react-dom';
+// import Portal from '../Portal';
+
+const Dialog = () =>
+  <div>
+    Hello World
+  </div>
+
+export default Dialog
