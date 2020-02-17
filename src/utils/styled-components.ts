fd985d0818892bfb73bc6ff842c1e9e4f539f9b3
Harrison McGuire <hmcguire13@gmail.com>
2020-02-17 12:47:25 -0500

    styled module line

--- src/utils/styled-components.ts
+++ src/utils/styled-components.ts
@@ -7,9 +7,7 @@ const {
   createGlobalStyle,
   keyframes,
   ThemeProvider
-} = styledComponents as styledComponents.ThemedStyledComponentsModule<
-  ThemeInterface
->
+} = styledComponents as styledComponents.ThemedStyledComponentsModule<ThemeInterface>
 
 export { css, createGlobalStyle, keyframes, ThemeProvider }
 export default styled
