4f82c8427fdefbfaea3802fcc26abd4c0ae1ad39
Harrison <hmcguire13@gmail.com>
2020-02-28 04:13:00 -0500

    neu button style

--- src/NButton/stories.tsx
+++ src/NButton/stories.tsx
@@ -9,11 +9,16 @@ stories.addDecorator(withKnobs)
 
 const noop = () => {}
 
-stories
-  .add('with text', () => (
-    <NButton
-      clicked={boolean('Clicked', false)}
-      disabled={boolean('Disabled', false)}
-      onClick={ noop }
-    />
-  ))
+const NeuButton = () => {
+  return (
+    <div style={{ background: '#dfdbdd', height: '500px', padding: '50px', borderRadius: '10px' }}>
+      <NButton
+        clicked={boolean('Clicked', false)}
+        disabled={boolean('Disabled', false)}
+        onClick={ noop }
+      />
+    </div>
+  )
+}
+
+stories.add('with text', () => <NeuButton />)
